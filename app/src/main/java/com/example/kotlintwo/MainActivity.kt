38166665e3659ed29ef438de4356d0995f4b92f5
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2023-10-11 11:36:54 +0500

    updated

--- app/src/main/java/com/example/kotlintwo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintwo/MainActivity.kt
@@ -2,12 +2,29 @@ package com.example.kotlintwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var DiceView: TextView
+    lateinit var RollBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        DiceView = findViewById(R.id.dice_view)
+        RollBtn = findViewById(R.id.roll_btn)
+
+        RollBtn.setOnClickListener {
+            rolldice()
+        }
+    }
+
+    private fun rolldice() {
+        val random_value = Random.nextInt(6) + 1
+        DiceView.text = random_value.toString()
     }
 }
