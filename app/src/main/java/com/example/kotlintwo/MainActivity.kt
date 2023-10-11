71b1b81fb89d6a991c068223a033d19b0f7c25c5
abdullahbinshahzad <abdullahbinshahzad228@gmail.com>
2023-10-11 15:53:49 +0500

    updated

--- app/src/main/java/com/example/kotlintwo/MainActivity.kt
+++ app/src/main/java/com/example/kotlintwo/MainActivity.kt
@@ -3,28 +3,34 @@ package com.example.kotlintwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var DiceView: TextView
-    lateinit var RollBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DiceView = findViewById(R.id.dice_view)
-        RollBtn = findViewById(R.id.roll_btn)
+        val rollBtn: Button = findViewById(R.id.roll_btn)
 
-        RollBtn.setOnClickListener {
+        rollBtn.setOnClickListener {
             rolldice()
         }
     }
 
     private fun rolldice() {
-        val random_value = Random.nextInt(6) + 1
-        DiceView.text = random_value.toString()
+        val diceView: ImageView = findViewById(R.id.dice_view)
+
+        val random_image = when(Random.nextInt(6) + 1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceView.setImageResource(random_image)
     }
 }
